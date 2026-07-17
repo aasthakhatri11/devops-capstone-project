2b24cd243c851ac52c854a5285f2180e6b50e728
Aastha Khatri <aasthakhatri05@gmail.com>
2026-07-17 13:12:34 -0400

    Move Talisman initialization

--- service/__init__.py
+++ service/__init__.py
@@ -14,6 +14,8 @@ from flask_talisman import Talisman
 app = Flask(__name__)
 app.config.from_object(config)
 
+Talisman(app)
+
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
 from service import routes, models  # noqa: F401 E402
